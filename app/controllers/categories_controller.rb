d28dec33dfd7b28615857e88038d01357b3bbb6e
minisera <koki.ohtaka0821@gmail.com>
2020-12-18 17:04:45 +0900

    first-commit

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,13 @@
+class CategoriesController < ApplicationController
+  def index
+  end
+
+  def create
+    category = Category.new(category_params)
+  end
+
+private
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
